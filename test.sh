a78f57eece36066b8f5dcd6fa5546f4a24276394
Felix Rodriguez <felix.a.rod@gmail.com>
2015-09-16 16:13:49 -0700

    try again

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 git diff-tree --no-commit-id --name-only -r $(git rev-parse HEAD) | grep "Dockerfile"
 if [ $? -eq 0 ]
 then 
-  curl -L https://github.com/docker/compose/releases/download/1.4.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
+  sudo curl -L https://github.com/docker/compose/releases/download/1.4.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
   docker-compose up && docker-compose logs | grep "Error bounds in tester too tight."
 else
   echo "skipping build" 
