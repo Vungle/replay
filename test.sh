58cbbcb8cf994350aaa881503d4802c107cbdf09
Felix Rodriguez <felix.a.rod@gmail.com>
2015-09-17 12:31:04 -0700

    test

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ git diff-tree --no-commit-id --name-only -r $(git rev-parse HEAD) | grep "Docker
 if [ $? -eq 0 ]
 then 
   sudo curl http://www.netlib.org/blacs/BLACS/DOWNLOAD/ERROR.LOG >> log/example.log
-  curl -L https://github.com/docker/compose/releases/download/1.4.1/docker-compose-`uname -s`-`uname -m` > docker-compose && sudo chmod +x docker-compose
+  curl -L https://github.com/docker/compose/releases/download/1.3.1/docker-compose-`uname -s`-`uname -m` > docker-compose && sudo chmod +x docker-compose
   ./docker-compose up && ./docker-compose logs | grep "Error bounds in tester too tight."
 else
   echo "skipping build" 
