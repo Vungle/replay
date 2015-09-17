d1b4749633eba6aa8b3a708cb268c21185fb86ec
Felix Rodriguez <felix.a.rod@gmail.com>
2015-09-17 13:54:04 -0700

    should pass now

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ if [ $? -eq 0 ]
 then 
   sudo curl http://www.netlib.org/blacs/BLACS/DOWNLOAD/ERROR.LOG >> log/example.log
   curl -L https://github.com/docker/compose/releases/download/1.3.1/docker-compose-`uname -s`-`uname -m` > docker-compose && sudo chmod +x docker-compose
-  ./docker-compose up -d && docker logs --tail="100" $(docker ps | grep "vungle/replay" | awk '{ print $1}') | grep "Mixed rs6k and anything else generates errors in SINGLE and SINGLE COMPLEX"
+  ./docker-compose up -d && sleep 5 && docker logs --tail="all" $(docker ps | grep "vungle/replay" | awk '{ print $1}') 2>&1 | grep -q "All problems fixed"
 else
   echo "skipping build" 
 fi
