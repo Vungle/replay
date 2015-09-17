fe6d45ac2f8f3642a14bb944d4505cae6abe1c2a
Felix Rodriguez <felix.a.rod@gmail.com>
2015-09-17 12:42:56 -0700

    only show a batch of logs

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM vungle/heka
 MAINTAINER Felix Rodriguez <felix.rodriguez@vungle.com>
 COPY conf.toml /etc/heka/
+
--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ if [ $? -eq 0 ]
 then 
   sudo curl http://www.netlib.org/blacs/BLACS/DOWNLOAD/ERROR.LOG >> log/example.log
   curl -L https://github.com/docker/compose/releases/download/1.3.1/docker-compose-`uname -s`-`uname -m` > docker-compose && sudo chmod +x docker-compose
-  ./docker-compose up && ./docker-compose logs | grep "Error bounds in tester too tight."
+  ./docker-compose up -d && ./docker-compose logs --tail="300" | grep "Error bounds in tester too tight."
 else
   echo "skipping build" 
 fi
