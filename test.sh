782014ae1505b8b77c8d8852dd0c6da959745d93
Felix Rodriguez <felix.a.rod@gmail.com>
2015-09-17 14:07:52 -0700

    always test

--- test.sh
+++ test.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-git diff-tree --no-commit-id --name-only -r $(git rev-parse HEAD) | grep "Dockerfile"
-if [ $? -eq 0 ]
-then 
-  sudo curl http://www.netlib.org/blacs/BLACS/DOWNLOAD/ERROR.LOG >> log/example.log
-  curl -L https://github.com/docker/compose/releases/download/1.3.1/docker-compose-`uname -s`-`uname -m` > docker-compose && sudo chmod +x docker-compose
-  ./docker-compose up -d && sleep 5 && docker logs --tail="all" $(docker ps | grep "vungle/replay" | awk '{ print $1}') 2>&1 | grep -q "All problems fixed"
-else
-  echo "skipping build" 
-fi
+sudo curl http://www.netlib.org/blacs/BLACS/DOWNLOAD/ERROR.LOG >> log/example.log
+curl -L https://github.com/docker/compose/releases/download/1.3.1/docker-compose-`uname -s`-`uname -m` > docker-compose && sudo chmod +x docker-compose
+./docker-compose up -d && sleep 5 && docker logs --tail="all" $(docker ps | grep "vungle/replay" | awk '{ print $1}') 2>&1 | grep -q "All problems fixed"
