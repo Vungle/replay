177608eff5b1c55c58d3cea35e3ca978d8434d2d
Felix Rodriguez <felix.a.rod@gmail.com>
2015-09-17 14:03:23 -0700

    always build since PR's are being skipped

--- build.sh
+++ build.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-git diff-tree --no-commit-id --name-only -r $(git rev-parse HEAD) | grep "Dockerfile"
-if [ $? -eq 0 ]
-then 
-  echo "Building ..."
-  docker build -t vungle/replay .
-else
-  echo "skipping build" 
-fi
+echo "Building ..."
+docker build -t vungle/replay .
--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-git diff-tree --no-commit-id --name-only -r $(git rev-parse HEAD) | grep "Dockerfile" 
-if [ $? -eq 0 ]
-then
-  docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-  if $CIRCLE_BRANCH == "master"
-  then 
-    echo "Pushing to latest"
-    docker push vungle/replay
-  else
-    echo "Pushing to tag $CIRCLE_SHA1"
-    docker build -t vungle/replay:$CIRCLE_SHA1 .
-    docker push vungle/replay:$CIRCLE_SHA1
-  fi
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+if $CIRCLE_BRANCH == "master"
+then 
+  echo "Pushing to latest"
+  docker push vungle/replay
 else
-  echo "skipping push"
+  echo "Pushing to tag $CIRCLE_SHA1"
+  docker build -t vungle/replay:$CIRCLE_SHA1 .
+  docker push vungle/replay:$CIRCLE_SHA1
 fi
