86d3f0e2412e91fece93a4ceaafb9af0608cd920
Felix Rodriguez <felix.a.rod@gmail.com>
2015-09-17 13:14:01 -0700

    have to restructure build so docker-compose works

--- build.sh
+++ build.sh
@@ -2,14 +2,8 @@
 git diff-tree --no-commit-id --name-only -r $(git rev-parse HEAD) | grep "Dockerfile"
 if [ $? -eq 0 ]
 then 
-  if $CIRCLE_BRANCH == "master"
-  then
-    echo "Building latest..."
-    docker build -t vungle/replay .
-  else
-    echo "Building $CIRCLE_SHA1"
-    docker build -t vungle/replay:$CIRCLE_SHA1 . 
-  fi
+  echo "Building ..."
+  docker build -t vungle/replay .
 else
   echo "skipping build" 
 fi
